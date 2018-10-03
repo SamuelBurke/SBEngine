7de2de56d3d7829654427e18705cc0f86161c126
SamuelBurke <43615636+SamuelBurke@users.noreply.github.com>
2018-10-03 16:20:18 +0100

    Add files via upload

--- src/SBEngine/Entity.h
+++ src/SBEngine/Entity.h
@@ -12,29 +12,29 @@ public:
 
 	std::shared_ptr<Core> GetCore();
 
-	template <class T>
-	std::shared_ptr<T> AddComponent<T>()
-	{
+	//template <typename T>
+	//std::shared_ptr<T> AddComponent<T>()
+	//{
 
-	}
+	//}
 
-	template <class T>
-	std::shared_ptr<T> AddComponent<T, A>(a:A)
-	{
+	//template <class T>
+	//std::shared_ptr<T> AddComponent<T, A>(a:A)
+	//{
 
-	}
+	//}
 
-	template <class T>
-	std::shared_ptr<T> AddComponent<T, A, B>(a:A, b:B)
-	{
+	//template <class T>
+	//std::shared_ptr<T> AddComponent<T, A, B>(a:A, b:B)
+	//{
 
-	}
+	//}
 
-	template <class T>
-	std::shared_ptr<T> AddComponent<T, A, B, C>(a:A, b:B, c:C)
-	{
+	//template <class T>
+	//std::shared_ptr<T> AddComponent<T, A, B, C>(a:A, b:B, c:C)
+	//{
 
-	}
+	//}
 
 private:
 	std::weak_ptr<Core> core;
--- src/test_engine/main.cpp
+++ src/test_engine/main.cpp
@@ -1,13 +1,42 @@
+#include <SDL2/SDL.h>
 #include <iostream>
+#include <exception>
 
 #include "\SBEngine\src\SBEngine\Component.h"
 
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+	//Component::GetEntity
 
-	
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		throw std::exception();
+	}
 
-	getchar();
+	SDL_Window *window = SDL_CreateWindow("Test Engine",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE);
+
+	bool quit = false;
+
+	while (!quit)
+	{
+		SDL_Event event = { 0 };
+
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				quit = true;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
 	return 0;
 }
